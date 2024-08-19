61c04e83dbf281a2c4085bb51c7cc48e4ea47660
angle4205 <angomez4205@gmail.com>
2024-08-18 21:20:13 -0400

    Calendar and reminders small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,6 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  {
-    path: 'reminders',
-    loadChildren: () => import('./reminders/reminders.module').then( m => m.RemindersPageModule)
-  },
-  {
-    path: 'calendar',
-    loadChildren: () => import('./calendar/calendar.module').then( m => m.CalendarPageModule)
-  },
 ];
 @NgModule({
   imports: [
