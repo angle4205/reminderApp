452e9708dde61761e35485e90ec5cfda65b5ce47
angle4205 <angomez4205@gmail.com>
2024-09-08 22:52:26 -0300

    login page design and validations

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username: string = ""
+  password: string = ""
+
+  constructor(private router: Router, public alert: ToastController) { }
+
+  redirectHome() {
+    this.router.navigate(['/tabs/home']);
+  }
+
+  redirectSignup() {
+    this.router.navigate(['/signup']);
+  }
+
+  async missingInputsToast() {
+    const alert = await this.alert.create({
+      message: 'Missing inputs',
+      duration: 2000,
+      icon: 'sad'
+    });
+    await alert.present();
+  }
+
+  async usernameErrorToast() {
+    const alert = await this.alert.create({
+      message: 'Username cannot contain more than 25 characters',
+      duration: 4000,
+      icon: 'sad'
+    });
+    await alert.present();
+  }
+
+  logIn() {
+    if (this.username.trim() === "") {
+      console.log("Missing username");
+      this.missingInputsToast();
+    } else if (this.password.trim() === "") {
+      console.log("Missing password");
+      this.missingInputsToast();
+    } else if (this.username.length > 25) {
+      console.log("Username too long")
+      this.usernameErrorToast()
+    }
+    else {
+      console.log("User logged in succesfully")
+      this.redirectHome()
+    }
+  }
 
   ngOnInit() {
   }
