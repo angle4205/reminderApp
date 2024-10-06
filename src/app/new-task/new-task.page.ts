adcef3d668a4761066b33ac19995095e112ad51f
angle4205 <angomez4205@gmail.com>
2024-10-06 00:59:22 -0300

    modified new task creator with new weekly / location based tasks notifications

--- src/app/new-task/new-task.page.ts
+++ src/app/new-task/new-task.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-task.page.scss'],
 })
 export class NewTaskPage implements OnInit {
-  
+  selectedDays: string[] = [];
+  repeatWeekly: boolean = false;
+  locationBased: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  repeatSelect() {
+    this.repeatWeekly = this.selectedDays.length > 0;
+    this.locationBased = this.selectedDays.includes('location');
   }
 
 }
