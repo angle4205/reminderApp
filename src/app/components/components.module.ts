5dbffd59cb354f758ef2ab8bc9cbe63a4d6bd007
angle4205 <angomez4205@gmail.com>
2024-10-17 13:34:21 -0300

    added redirect method to configuration button in header

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ShareModule } from '../modules/share/share.module';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ShareModule,
+    IonicModule
   ]
 })
 export class ComponentsModule { }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class HeaderComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
   @Input() title:String=""
 
   ngOnInit() {}
+
+  redirectConfiguration() {
+    this.router.navigate(['configuration'])
+  }
 
 }
