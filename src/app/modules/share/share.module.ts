75a7efaf497115b94b1a973709a936c90c742275
angle4205 <angomez4205@gmail.com>
2024-10-17 13:50:33 -0300

    Transformed user-card into component for better perfomance

--- src/app/modules/share/share.module.ts
+++ src/app/modules/share/share.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { UserCardComponent } from 'src/app/components/user-card/user-card.component';
 import { IonicModule } from '@ionic/angular';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports:[HeaderComponent,
-          IonicModule],
-  imports: [
-    CommonModule,
-    IonicModule
-  ]
+  declarations: [HeaderComponent, UserCardComponent],
+  exports: [HeaderComponent, UserCardComponent, IonicModule],
+  imports: [CommonModule, IonicModule],
 })
-export class ShareModule { }
+export class ShareModule {}
