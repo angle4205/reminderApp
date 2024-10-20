4e7318bf3cc211964ef408868bfe3f3a3c103db0
angle4205 <angomez4205@gmail.com>
2024-10-20 20:13:16 -0300

    added quotes card component

--- src/app/modules/share/share.module.ts
+++ src/app/modules/share/share.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { UserCardComponent } from 'src/app/components/user-card/user-card.component';
+import { QuotesCardComponent } from 'src/app/components/quotes-card/quotes-card.component';
 import { IonicModule } from '@ionic/angular';
 
 @NgModule({
-  declarations: [HeaderComponent, UserCardComponent],
-  exports: [HeaderComponent, UserCardComponent, IonicModule],
+  declarations: [HeaderComponent, UserCardComponent, QuotesCardComponent],
+  exports: [HeaderComponent, UserCardComponent, QuotesCardComponent, IonicModule],
   imports: [CommonModule, IonicModule],
 })
-export class ShareModule {}
+export class ShareModule { }
