43effbb7ac9359c7d950593e0d8f920cf2bd327a
Nicovt0 <nicovt2005@gmail.com>
2024-11-29 17:00:39 -0300

    Apk Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAJAr_BqWvEyYCdpUQeQ-7r4zzXKczDW-w",
+    authDomain: "focusflow-6f574.firebaseapp.com",
+    projectId: "focusflow-6f574",
+    storageBucket: "focusflow-6f574.appspot.com",
+    messagingSenderId: "553691589264",
+    appId: "1:553691589264:web:35061186de536c4098c615"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig:{
     apiKey: "AIzaSyAJAr_BqWvEyYCdpUQeQ-7r4zzXKczDW-w",
     authDomain: "focusflow-6f574.firebaseapp.com",
